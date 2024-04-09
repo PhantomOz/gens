ce672aff42b80431e3e3b1e149040c018d7e2f56
PhantomOz <faniogor@gmail.com>
2024-04-09 07:34:22 +0100

    This code adds missing functionalities

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -2,10 +2,9 @@
 pragma solidity ^0.8.19;
 
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import {ERC20Burnable} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Gen is ERC20, Ownable, ERC20Burnable {
+contract Gen is ERC20, Ownable {
     constructor(
         string memory _name,
         string memory _symbol
@@ -13,5 +12,17 @@ contract Gen is ERC20, Ownable, ERC20Burnable {
 
     function mint(address _account, uint256 _value) public onlyOwner {
         _mint(_account, _value);
+    }
+
+    function burn(uint256 _amount) external {
+        _burn(msg.sender, _amount);
+    }
+
+    function transfer(
+        address to,
+        uint256 value
+    ) public override returns (bool) {
+        _transfer(msg.sender, to, value);
+        return true;
     }
 }
